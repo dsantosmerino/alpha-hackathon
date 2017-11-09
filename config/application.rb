63125adb562ed4a3ff7e24460bc4f0bb350cf5ec
David Santos Merino <dsantosmerino@gmail.com>
2017-11-09 14:29:45 +0100

    Allow CORS requests

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,14 @@ module AlphaHackaton
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+        headers: :any,
+        methods: [:get, :post, :delete, :put, :options],
+        max_age: 0
+      end
+    end
   end
 end
