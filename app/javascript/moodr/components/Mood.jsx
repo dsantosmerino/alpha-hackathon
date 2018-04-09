443563d1ea64e14ecffc26a5b607da931d9d2cee
Gorka Pit <gorkapit@gmail.com>
2018-04-09 16:39:45 +0200

    Fix emoji spacing in mobile (hi Jose!)

--- app/javascript/moodr/components/Mood.jsx
+++ app/javascript/moodr/components/Mood.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
-import * as actions from '../actions';
+import * as actions from "../actions";
 
 export default connect(null, actions)(({ emoji, selectMood }) => {
   return (
-    <button className="mood-button" onClick={ () => selectMood(emoji) }>
-      { emoji }
+    <button className="mood-button" onClick={() => selectMood(emoji)}>
+      {emoji}
     </button>
-  )
+  );
 });
